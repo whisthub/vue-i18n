49179cfa8f6ee5c42e9328698bc0131cc47492db
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-05-31 21:03:16 +0200

    Add support for options api

--- test/components/options-api.vue
+++ test/components/options-api.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
 		<p class="welcome">{{ $t('welcome') }}</p>
+		<p class="greeting">{{ $t('greeting', { name }) }}</p>
 		<p class="bananas">{{ $tc('bananas', 10) }}</p>
 		<p class="no-bananas">{{ $tc('bananas', 0) }}</p>
 	</div>
@@ -17,8 +18,14 @@ export default {
 					'One banana',
 					i => i`${'n'} bananas`,
 				],
+				greeting: i => i`Hello ${'name'}!`,
 			},
 		},
+	},
+	data() {
+		return {
+			name: 'Whisthub',
+		};
 	},
 };
 </script>
---
+++ test/options-api-test.js
@@ -0,0 +1,35 @@
+// # options-api-test.js
+import { expect } from 'chai';
+import { createI18n } from '@whisthub/vue-i18n';
+import { useMount } from './utils/use-mount.js';
+import OptionsComponent from './components/options-api.vue';
+
+describe('The options api', function() {
+
+	before(useMount());
+
+	before(function() {
+		this.setup = function() {
+			const i18n = createI18n({
+				locale: 'en',
+				fallbackLocale: 'en',
+			});
+			return this.mount(OptionsComponent, {
+				global: {
+					plugins: [i18n],
+				},
+			});
+		};
+	});
+
+	it('renders correctly', function() {
+
+		let { $ } = this.setup();
+		expect($('p.welcome').textContent).to.equal('Welcome!');
+		expect($('p.greeting').textContent).to.equal('Hello Whisthub!');
+		expect($('p.bananas').textContent).to.equal('10 bananas');
+		expect($('p.no-bananas').textContent).to.equal('No bananas');
+
+	});
+
+});
