1d7fda706f4d14a43be292391b5acf9a654df157
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-05-31 19:04:53 +0200

    Support pluralization in `<i18n-t>` component

--- test/components/i18n-test.vue
+++ test/components/i18n-test.vue
@@ -1,22 +1,54 @@
 <template>
 	<div>
-		<div class="bare">{{ t('greeting', ['Whisthub']) }}</div>
-		<div class="greeting">
+		<p class="fragment">
 			<i18n-t keypath="greeting">
 				<span>Whisthub</span>
 			</i18n-t>
-		</div>
-		<div class="foo"></div>
+		</p>
+		<i18n-t tag="p" class="tag" keypath="greeting">
+			<b>{{ t('stranger') }}</b>
+		</i18n-t>
+		<i18n-t tag="p" class="named" keypath="named">Whisthub</i18n-t>
+		<i18n-t
+			tag="p"
+			class="plural"
+			keypath="bananas"
+			:plural="count"
+		>
+			<template #n>
+				<b>{{ count }}</b>
+			</template>
+		</i18n-t>
 	</div>
 </template>
 
 <script setup>
+import { ref, watch } from 'vue';
 import { useI18n } from '@whisthub/vue-i18n';
+
 const { t } = useI18n({
 	messages: {
 		en: {
 			greeting: i => i`Hello ${0}!`,
+			stranger: 'stranger',
+			named: i => i`Hello ${'name'}!`,
+		},
+		nl: {
+			named: i => i`Hallo ${'name'}!`,
+			bananas: [
+				i => i`Geen bananen`,
+				i => i`Eén banaan`,
+				i => i`${'n'} bananen`,
+			],
 		},
 	},
 });
+
+const count = ref(0);
+
+function increment() {
+	count.value = count.value + 1;
+}
+
+defineExpose({ increment });
 </script>
--- test/i18n-component-test.js
+++ test/i18n-component-test.js
@@ -1,4 +1,5 @@
 // # i18n-component-test.js
+import { nextTick } from 'vue';
 import { mount } from '@vue/test-utils';
 import { expect } from 'chai';
 import { createI18n } from '@whisthub/vue-i18n';
@@ -9,7 +10,9 @@ describe('The <i18n-t> component', function() {
 	before(function() {
 		this.mount = function() {
 			const i18n = createI18n({
-				locale: 'en',
+				locale: 'nl',
+				fallbackLocale: 'en',
+				silentFallbackWarn: true,
 			});
 			let view = mount(TestComponent, {
 				global: {
@@ -18,14 +21,46 @@ describe('The <i18n-t> component', function() {
 				props: {},
 			});
 			let { vm } = view;
-			return { view, vm, i18n };
+			return {
+				view,
+				vm,
+				i18n,
+				$(selector) {
+					return vm.$el.querySelector(selector);
+				},
+				$$(selector) {
+					return vm.$el.querySelectorAll(selector);
+				},
+			};
 		};
 	});
 
-	it('is mounted correctly', function() {
+	it('renders correctly', function() {
 
-		let { vm } = this.mount();
-		expect(vm.$el.querySelector('.greeting').innerHTML).to.equal('Hello <span>Whisthub</span>!');
+		let { $ } = this.mount();
+		expect($('p.fragment').innerHTML).to.equal('Hello <span>Whisthub</span>!');
+		expect($('p.tag').innerHTML).to.equal('Hello <b>stranger</b>!');
+		expect($('p.named').innerHTML).to.equal('Hallo Whisthub!');
+
+	});
+
+	it('pluralization', async function() {
+
+		let { $, vm } = this.mount();
+		expect($('p.plural').textContent).to.equal('Geen bananen');
+
+		vm.increment();
+		await nextTick();
+		expect($('p.plural').textContent).to.equal('Eén banaan');
+
+		vm.increment();
+		await nextTick();
+		expect($('p.plural').textContent).to.equal('2 bananen');
+
+		vm.increment();
+		vm.increment();
+		await nextTick();
+		expect($('p.plural').textContent).to.equal('4 bananen');
 
 	});
 
