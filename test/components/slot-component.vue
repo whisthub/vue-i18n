5ea1d0cbb0d6b3ba9c5c21993bd32cabfd82650a
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-11-08 11:10:25 +0100

    Fix #6: use correct parent scope for <i18n-t>

--- test/components/slot-component.vue
+++ test/components/slot-component.vue
@@ -3,3 +3,18 @@
 		<slot name="content"></slot>
 	</p>
 </template>
+
+<script setup>
+import { useI18n } from '@whisthub/vue-i18n';
+
+useI18n({
+	messages: {
+		en: {
+			greeting: i => i`Go away, ${0}`,
+		},
+		nl: {
+			greeting: i => i`Ga weg, ${0}`,
+		},
+	},
+});
+</script>
